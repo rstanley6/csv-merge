33ddff7a4f40f0f99a5fdee91453495f11a32ee8
Robert Stanley <rstanley6@outlook.com>
2017-08-26 16:32:25 -0500

    Added test .csv and printed out Row

--- ReadCSV.py
+++ ReadCSV.py
@@ -1,6 +1,11 @@
 import csv
 
+#csv_list = list()
+
 with open('my.csv', newline='') as csv_file:
     reader = csv.reader(csv_file)
     for row in reader:
-        print(str(row))
+        print(row[0])
+        print(row[1])
+        print(row[2])
+        print(row[3])
